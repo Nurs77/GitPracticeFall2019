cbe1b09cc365caafc9a3cf4c843537e084fa1308
Nursultan Tolebergen <nursultan.tolepbergen@gmail.com>
2020-02-29 15:27:08 -0500

    changes were made

--- src/day01_GitIntroTerminal/StringPractice.java
+++ src/day01_GitIntroTerminal/StringPractice.java
@@ -3,5 +3,6 @@ package day01_GitIntroTerminal;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Nursultan is the boss");
     }
 }
